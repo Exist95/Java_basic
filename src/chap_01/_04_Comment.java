ed80b7db090cb2cc635a3c05abc2b33da4d13f99
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-26 07:38:59 +0900

    Java 1-5

--- src/chap_01/_04_Comment.java
+++ src/chap_01/_04_Comment.java
@@ -10,9 +10,11 @@ public class _04_Comment {
         size = size + 10;
         System.out.println("신발 사이즈 " + size + "으로 보여주세요.");
 
-        /*int a = 10;
+        // 한 줄 주석 단축키 : 컨트롤 + /
+        // 여러 줄 주석 단축키 : 컨트롤 + 쉬프트 + /
+        int a = 10;
         int b = 20;
         System.out.println(a + b); //30
-         */
+
     }
 }
