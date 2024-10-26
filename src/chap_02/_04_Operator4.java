1c4822707df09b81e6836115764ed301528e48b2
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-26 14:01:01 +0900

    Java 2-4

---
+++ src/chap_02/_04_Operator4.java
@@ -0,0 +1,11 @@
+package chap_02;
+
+public class _04_Operator4 {
+    // 논리 연산자
+    boolean 김치찌개 = true;
+    boolean 계란말이 = true;
+    boolean 제육볶음 = true;
+    
+    // 자바스크립트랑 다를 거 없어서 패스
+    
+}
