87610eff03807cc053ebe06517b5409fba0d6c9c
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-26 14:02:19 +0900

    Java 2-5

---
+++ src/chap_02/_05_Operator5.java
@@ -0,0 +1,12 @@
+package chap_02;
+
+public class _05_Operator5 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        int max = (x>y) ? x : y;
+        System.out.println(max);
+        int min = (x<y) ? x : y;
+        System.out.println(min);
+    }
+}
