97e0f2047f5d4b9292aaedc944aa5ed77ab7925e
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-26 13:20:57 +0900

    Java 2-1

---
+++ src/chap_02/_01_Operator1.java
@@ -0,0 +1,19 @@
+package chap_02;
+
+public class _01_Operator1 {
+    public static void main(String[] args) {
+        // 산술 연산자
+        // 일반 연산
+        // 증감 연산
+        int val;
+        val = 10;
+        System.out.println(val); //10
+        System.out.println(++val); //11
+        System.out.println(val); //11
+
+        val = 10;
+        System.out.println(val); //10
+        System.out.println(val++); //10
+        System.out.println(val); //11
+    }
+}
