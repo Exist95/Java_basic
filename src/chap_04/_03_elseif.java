a7b8d7bd7bcf5b8c66e83167521714f4cec030ab
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-27 02:18:18 +0900

    Java 4-3

---
+++ src/chap_04/_03_elseif.java
@@ -0,0 +1,17 @@
+package chap_04;
+
+public class _03_elseif {
+    public static void main(String[] args) {
+        boolean hallabongAde = true;
+        boolean mangoJuice = true;
+
+        if(hallabongAde) {
+            System.out.println("한라봉 에이드 +1");
+        } else if (mangoJuice) {
+            System.out.println("망고 주수 +1");
+        } else {
+            System.out.println("아이스 아메리카노 +1");
+        }
+        System.out.println("커피 주문 완료 #1");
+    }
+}
