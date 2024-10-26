af263db67e146f35e605a6edc5c86fd722f17bc5
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-26 14:03:56 +0900

    Java 2-Quiz

---
+++ src/chap_02/_Quiz_02.java
@@ -0,0 +1,9 @@
+package chap_02;
+
+public class _Quiz_02 {
+    public static void main(String[] args) {
+        int height = 115;
+        String result = (height>=120) ? "탑습 가능합니다." : "탑승 불가능합니다.";
+        System.out.println(result);
+    }
+}
