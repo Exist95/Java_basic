3a63d2db4afb1ca23ca02773ac4c59a82106df74
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-31 07:39:27 +0900

    Java BinaryExam

---
+++ src/fastcampus/BinaryExam.java
@@ -0,0 +1,13 @@
+package fastcampus;
+
+public class BinaryExam {
+    public static void main(String[] args) {
+        // Q. 123을 10진수, 2진수, 8진수, 16진수로 출력하시오.
+
+        int decimal = 69;
+        System.out.println("decimal = " + decimal);
+        System.out.println("binary = " + Integer.toBinaryString(decimal));
+        System.out.println("octal = " + Integer.toOctalString((decimal)));
+        System.out.println("hexa = " + Integer.toHexString(decimal));
+    }
+}
