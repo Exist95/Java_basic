26ba9544dcacc7ce797996e03fbcf8f4542b0984
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-31 11:55:38 +0900

    Java _13_ArrayBasicTest

---
+++ src/fastcampus/_13_ArrayBasicTest.java
@@ -0,0 +1,16 @@
+package fastcampus;
+
+public class _13_ArrayBasicTest {
+    // 동일한 데이터 여러 개를 연속적으로 만드는 방법 => 배열
+
+    public static void main(String[] args) {
+        // Q. 정수 3개를 저장 할 변수를 선언하고 모두 10을 저장하세요.
+        // 데이터 수가 늘어나면 [비효율적]이고 [데이터처리가 복잡]해진다.
+        int a, b, c;
+        a=10;
+        b=10;
+        c=10;
+        System.out.println(a+b+c);
+
+    }
+}
