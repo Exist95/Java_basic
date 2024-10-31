232fdd539df5387273bdf452a5476f4908469c0f
노종열 <96723716+Exist95@users.noreply.github.com>
2024-10-31 13:37:40 +0900

    Java _15_ArrayTest

---
+++ src/fastcampus/_15_ArrayTest.java
@@ -0,0 +1,19 @@
+package fastcampus;
+
+public class _15_ArrayTest {
+    public static void main(String[] args) {
+
+        int[] a = {10,20,30,40,50};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        int sum = a[0] + a[3];
+        System.out.println(sum);
+        System.out.println(a.length);
+    }
+}
