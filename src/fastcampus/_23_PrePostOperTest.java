19f7caccfb2b14df757b557433182925862e994d
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-01 09:30:23 +0900

    Java _23_PrePostOperTest

---
+++ src/fastcampus/_23_PrePostOperTest.java
@@ -0,0 +1,12 @@
+package fastcampus;
+
+public class _23_PrePostOperTest {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(--x); //9
+        System.out.println(x++); //9
+        System.out.println(x--); //10
+        System.out.println(++x); //10
+        System.out.println(x); //10
+    }
+}
