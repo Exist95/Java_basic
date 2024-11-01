f7937e3b9c3de948ab2b377286c7102bf11d17ca
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-02 07:21:35 +0900

    Java _42_ApiOverloading

---
+++ src/fastcampus/_42_ApiOverloading.java
@@ -0,0 +1,18 @@
+package fastcampus;
+
+public class _42_ApiOverloading {
+    public static void main(String[] args) {
+        int max = maxValue(7,5);
+        System.out.println(max);
+        int min = minValue(4,2);
+        System.out.println(min);
+    }
+
+    public static int maxValue(int a, int b){
+        return (a > b) ? a : b;
+    }
+
+    public static int minValue(int a, int b){
+        return (a < b) ? a : b;
+    }
+}
