a7c98b14c0aa7e0624e05ec2037be63dcdeafede
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-01 09:42:07 +0900

    Java _26_AssignOperTest

---
+++ src/fastcampus/_26_AssignOperTest.java
@@ -0,0 +1,22 @@
+package fastcampus;
+
+public class _26_AssignOperTest {
+    public static void main(String[] args) {
+        // Q. 아래를 수행 후 결과 값은 얼마인가
+        // 1식 증가하는 수식을 써보세요.
+        int a = 1;
+        a++;
+        ++a;
+        a=a+1;
+        a+=1;
+        System.out.println("a" + a);
+
+        int b = 7;
+        b--;
+        --b;
+        b=b-1;
+        b-=1;
+        System.out.println("b" + b);
+
+    }
+}
