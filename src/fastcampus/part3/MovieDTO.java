f85630726e174ab5ee252ce4872a930097c1abc8
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-02 15:18:25 +0900

    Java MemberDTO

---
+++ src/fastcampus/part3/MovieDTO.java
@@ -0,0 +1,10 @@
+package fastcampus.part3;
+
+public class MovieDTO {
+    public String title;
+    public int day;
+    public String major;
+    public String part;
+    public float time;
+    public int level;
+}
