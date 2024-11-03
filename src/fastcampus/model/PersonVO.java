e211ac4e041db40de6fc0135fefc0fc3ef2010e1
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-04 08:10:52 +0900

    Java SetterGetterTest

--- src/fastcampus/model/PersonVO.java
+++ src/fastcampus/model/PersonVO.java
@@ -1,8 +1,33 @@
 package fastcampus.model;
 
 public class PersonVO {
-    public String name;
+    private String name;
     private int age;
-    public String phone;
+    private String phone;
 
+    // setter method
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    // getter method
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getPhone(){
+        return this.phone;
+    }
 }
--- src/fastcampus/part3/SetterGetterTest.java
+++ src/fastcampus/part3/SetterGetterTest.java
@@ -2,13 +2,14 @@ package fastcampus.part3;
 
 import fastcampus.model.PersonVO;
 
-public class PersonInfoHide {
+public class SetterGetterTest {
     public static void main(String[] args) {
-        // 한 사람의 회원 정보를 저장할 객체를 생성하시오.
         PersonVO vo = new PersonVO();
+
         vo.setName("홍");
         vo.setAge(123);
         vo.setPhone("010-");
         System.out.println(vo.getName() + "\t" + vo.getAge() + "\t" + vo.getPhone());
+
     }
 }
