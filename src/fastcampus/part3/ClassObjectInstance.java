dff488089145407b78ad6253642ef503335049cb
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-04 15:17:32 +0900

    Java ClassObjectInstance

---
+++ src/fastcampus/part3/ClassObjectInstance.java
@@ -0,0 +1,11 @@
+package fastcampus.part3;
+
+import fastcampus.model.Student;
+
+public class ClassObjectInstance {
+    public static void main(String[] args) {
+        Student st1; // st1 => Object 라고 부른다.
+        st1 = new Student("123","123",123,"123",123,"123");// => instance 라고 부른다.
+
+    }
+}
